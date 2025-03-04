ca253f50b879e807da735e679576b84c7ec211fa
Kayce <pat.nzediegwu@gmail.com>
2025-03-03 17:11:25 -0800

    feat: add search filter

---
+++ fetch-fe/src/components/SearchFilter.tsx
@@ -0,0 +1,75 @@
+import { useCallback, type Dispatch, type SetStateAction } from 'react'
+import { BreedComboBox } from './BreedComboBox'
+import { AgeSlider } from './AgeSlider'
+import type { DogSearchParams } from '@/api/dog.types'
+import { Button } from './ui/button'
+import { getDogBreeds } from '@/api/dog'
+import { useQuery } from '@tanstack/react-query'
+import { defaultFilters } from '@/hooks/useDogFilters'
+import { debounce } from '@/lib/utils'
+import { DELAY_MS } from '@/lib/constants'
+
+type Props = {
+  filters: Required<DogSearchParams>
+  setFilters: Dispatch<SetStateAction<Required<DogSearchParams>>>
+  totalResults: number
+}
+
+export const SearchFilter = ({ totalResults, filters, setFilters }: Props) => {
+  const { data: breeds = [] } = useQuery({
+    queryKey: ['dogBreeds'],
+    queryFn: getDogBreeds,
+  })
+
+  const range: [number, number] = [filters.ageMin, filters.ageMax]
+
+  const toggleSelection = useCallback(
+    (breedId: string) => {
+      setFilters((prev) => ({
+        ...prev,
+        breeds: prev.breeds?.includes(breedId)
+          ? prev.breeds.filter((id) => id !== breedId)
+          : [...(prev.breeds || []), breedId],
+      }))
+    },
+    [setFilters]
+  )
+
+  const handleSliderChange = useCallback(
+    (value: number) => {
+      const debouncedSetFilters = debounce<number>((value: number) => {
+        setFilters((prev) => ({ ...prev, ageMax: value }))
+      }, DELAY_MS)
+      debouncedSetFilters(value)
+    },
+    [setFilters]
+  )
+
+  const clearSelection = useCallback(() => {
+    setFilters(() => defaultFilters)
+  }, [setFilters])
+
+  return (
+    <div className="flex flex-col gap-6 p-4">
+      <p className="text-xl font-bold text-purple-400">Search Filters </p>
+      <span className="text-sm text-gray-500">
+        {totalResults > 0 && `${totalResults} results found`}
+      </span>
+      <BreedComboBox
+        items={breeds}
+        selectedItems={filters?.breeds ?? []}
+        onToggleSelection={toggleSelection}
+        onClearSelection={clearSelection}
+      />
+      <AgeSlider range={range} onSliderChange={handleSliderChange} />
+
+      <Button
+        className="cursor-pointer"
+        variant="outline"
+        onClick={clearSelection}
+      >
+        Reset Dog Filters
+      </Button>
+    </div>
+  )
+}
--- fetch-fe/src/lib/constants.ts
+++ fetch-fe/src/lib/constants.ts
@@ -1,8 +1,12 @@
 export const BASE_URL = 'https://frontend-take-home-service.fetch.com'
 export const APP_NAME = 'Pawradise'
 export const REPO_URL = 'https://github.com/kaayce/fetch-FE'
+export const MAX_DOG_AGE = 30
 export const PAGE_LIMIT = 20
+export const MAX_PAGE_LIMIT = 100
 export const HOUR = 60 * 60 * 1000
+export const DAY = 60 * 60 * 1000 * 24
+export const DELAY_MS = 300
 
 export const API_ENDPOINTS = {
   LOGIN: '/auth/login',
@@ -13,4 +17,10 @@ export const API_ENDPOINTS = {
   LOCATIONS: '/locations',
   LOCATION_SEARCH: '/locations/search',
   DOG_SEARCH: '/dogs/search',
+} as const
+
+export const PATHS = {
+  LOGIN: '/login',
+  HOME: '/',
+  MATCH: '/match',
 } as const
